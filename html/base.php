51c31b0ae97dc6944284a637874d01318d48da6b
Salvatore Laisa <moebiusmania@gmail.com>
2013-09-26 14:29:48 +0200

    file dei templates php

---
+++ html/base.php
@@ -0,0 +1,6 @@
+<?php 
+
+// Nuovo progetto
+
+
+?>
---
+++ html/html.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Prova</title>
+</head>
+<body>
+
+<?php 
+
+
+
+?>
+
+</body>
+</html>
